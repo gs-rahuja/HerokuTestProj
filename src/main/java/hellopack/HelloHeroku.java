41857b462f637e87d955d3e139fb3262cfeed1b8
gs-rahuja <rahuja@gainsight.com>
2015-01-19 15:21:18 +0530

    First Sample Project on Github

---
+++ src/main/java/hellopack/HelloHeroku.java
@@ -0,0 +1,11 @@
+package hellopack;
+
+public class HelloHeroku {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Heroku. Just started with you");
+	}
+
+}
